043753b2d1742191cffa1ff86db447b894277c6b
anchitkhare2198 <anchit.khare20@gmail.com>
2022-01-14 02:06:49 +0530

    Till Brush Size Dialog.

--- app/src/main/java/com/example/doodle/MainActivity.kt
+++ app/src/main/java/com/example/doodle/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.doodle
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener{
+            showBrushSizeDialog()
+        }
+    }
+
+    private fun showBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size : ")
+        brushDialog.show()
+        val small_btn = brushDialog.ib_small_brush
+        small_btn.setOnClickListener{
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val medium_btn = brushDialog.ib_medium_brush
+        medium_btn.setOnClickListener{
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val large_btn = brushDialog.ib_large_brush
+        large_btn.setOnClickListener{
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
     }
 }
