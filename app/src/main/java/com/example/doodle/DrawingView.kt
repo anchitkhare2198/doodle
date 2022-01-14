3c889c167177fadcf55969cac46cbfa20ede4da2
anchitkhare2198 <anchit.khare20@gmail.com>
2022-01-15 01:53:17 +0530

    Export Image Left.

--- app/src/main/java/com/example/doodle/DrawingView.kt
+++ app/src/main/java/com/example/doodle/DrawingView.kt
@@ -16,9 +16,17 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs) {
     private var color = Color.BLACK
     private var canvas: Canvas? = null
     private val mPaths = ArrayList<CustomPath>()
+    private val mUndoPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
+    }
+
+    fun onClickUndo(){
+        if(mPaths.size>0){
+            mUndoPaths.add(mPaths.removeAt(mPaths.size - 1))
+            invalidate()
+        }
     }
 
     private fun setUpDrawing(){
@@ -93,6 +101,11 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs) {
     fun setSizeForBrush(newSize:Float){
         mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,newSize,resources.displayMetrics)
         mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+    fun setColor(newColor:String){
+        color = Color.parseColor(newColor)
+        mDrawPaint!!.color = color
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
--- app/src/main/java/com/example/doodle/MainActivity.kt
+++ app/src/main/java/com/example/doodle/MainActivity.kt
@@ -1,19 +1,71 @@
 package com.example.doodle
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint:ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
+
         drawing_view.setSizeForBrush(20.toFloat())
         ib_brush.setOnClickListener{
             showBrushSizeDialog()
+        }
+
+        ib_gallery.setOnClickListener {
+            if(isReadStorageAllowed()){
+                val i = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(i,GALLERY)
+            }else{
+                RequestStoragePermission()
+            }
+        }
+
+        ib_undo.setOnClickListener {
+            drawing_view.onClickUndo()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == GALLERY){
+                try {
+                    if(data!!.data != null){
+                        iv_image_background.visibility = View.VISIBLE
+                        iv_image_background.setImageURI(data.data)
+                    }else{
+                        Toast.makeText(this, "This image is not supported", Toast.LENGTH_SHORT).show()
+                    }
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
@@ -37,5 +89,57 @@ class MainActivity : AppCompatActivity() {
             drawing_view.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
         }
+    }
+
+    fun paintClicked(view: View){
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+
+            val colorTag = imageButton.tag.toString()
+            drawing_view.setColor(colorTag)
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+            )
+            mImageButtonCurrentPaint = view
+        }
+    }
+
+    private fun RequestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())){
+            Toast.makeText(this, "Need Permission to set the Background Image", Toast.LENGTH_SHORT).show()
+
+        }
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean{
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object{
+        private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
